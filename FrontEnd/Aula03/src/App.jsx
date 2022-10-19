35d7ef6ca09b5835f1b722c47b9f75910644337e
dnwest <cristian.daniel.fernandes@gmail.com>
2022-10-19 20:55:24 -0300

    Aula03

---
+++ FrontEnd/Aula03/src/App.jsx
@@ -0,0 +1,11 @@
+import "./assets/app.css";
+
+function App() {
+  const nome = "West"
+  return (<>
+   <div><h1>Hello {nome}</h1></div>
+   <div><img alt="wold" src="https://thumbs.dreamstime.com/b/mapa-do-mundo-em-um-fundo-tecnologico-o-melhor-conceito-do-internet-do-neg%C3%B3cio-global-elementos-desta-imagem-fornecidos-perto-66529240.jpg"></img><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis sapiente adipisci provident nisi facilis neque tenetur. Quaerat, cupiditate. Ab voluptate incidunt officiis laudantium quae atque ducimus minus ut culpa ratione.</p></div>
+  </>);
+}
+
+export default App
