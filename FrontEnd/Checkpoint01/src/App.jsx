0e4599a7aeac8fba87e9ec27e26851dc3f9d0a6c
dnwest <cristian.daniel.fernandes@gmail.com>
2022-11-08 22:34:02 -0300

    CheckPoint01

---
+++ FrontEnd/Checkpoint01/src/App.jsx
@@ -0,0 +1,9 @@
+import { FormColor } from './FormColor';
+import './index.css'
+
+
+const App = ()=> 
+ <div>{<FormColor/>}</div>
+
+
+export default App;
---
+++ FrontEnd/Checkpoint01/src/FormColor.jsx
@@ -0,0 +1,6 @@
+import { Component } from "react";
+
+export class FormColor extends Component { 
+render(){ return(
+<form><h1 className="container">Olá Mundo</h1></form>)
+}}
