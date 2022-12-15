daf626896dd8272d0d447ccf7d9f575d0746f2ae
dnwest <cristian.daniel.fernandes@gmail.com>
2022-12-14 23:00:14 -0300

    LoginForm -  AuthContext debug Error

--- FrontEnd/Checkpoint02/src/Components/LoginForm.jsx
+++ FrontEnd/Checkpoint02/src/Components/LoginForm.jsx
@@ -1,22 +1,52 @@
+import { useContext } from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../Components/AuthContext";
+import api from "../api/api";
+import { NavBarContext } from "../contexts/NavBarContext";
+
 import styles from "./Form.module.css";
 
 const LoginForm = () => {
+  const [name, setUser] = useState("dentistaAdmin");
+  const [password, setPassword] = useState("admin123");
+
+  const navigate = useNavigate();
+  const { fillUsetDataState, setIsLogado } = useContext(AuthContext);
+  const { contextIsLight } = useContext(NavBarContext);
+
   const handleSubmit = (e) => {
-    //Nesse handlesubmit você deverá usar o preventDefault,
-    //enviar os dados do formulário e enviá-los no corpo da requisição 
-    //para a rota da api que faz o login /auth
-    //lembre-se que essa rota vai retornar um Bearer Token e o mesmo deve ser salvo
-    //no localstorage para ser usado em chamadas futuras
-    //Com tudo ocorrendo corretamente, o usuário deve ser redirecionado a página principal,com react-router
-    //Lembre-se de usar um alerta para dizer se foi bem sucedido ou ocorreu um erro
+    e.preventDefault();
+
+    auth();
   };
+
+  async function auth() {
+    try {
+      const response = await api.post("/auth", {
+        username: name,
+        password: password,
+      });
+      alert("Usuário logado com sucesso !");
+      fillUsetDataState({
+        token: response.data.token,
+      });
+      setIsLogado(true);
+
+      setTimeout(() => {
+        navigate("/home");
+      }, 2000);
+    } catch (erro) {
+      alert("Verifique suas informações novamente");
+    }
+  }
 
   return (
     <>
-      {/* //Na linha seguinte deverá ser feito um teste se a aplicação
-        // está em dark mode e deverá utilizar o css correto */}
       <div
-        className={`text-center card container ${styles.card}`}
+        className={`text-center container ${
+          contextIsLight ? styles.card : styles.cardDark
+        }`}
       >
         <div className={`card-body ${styles.CardBody}`}>
           <form onSubmit={handleSubmit}>
@@ -25,6 +55,8 @@ const LoginForm = () => {
               placeholder="Login"
               name="login"
               required
+              value={name}
+              onChange={(e) => setUser(e.target.value)}
             />
             <input
               className={`form-control ${styles.inputSpacing}`}
@@ -32,9 +64,15 @@ const LoginForm = () => {
               name="password"
               type="password"
               required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
-            <button className="btn btn-primary" type="submit">
-              Send
+            <button
+              className="btn btn-primary"
+              type="submit"
+              disabled={name.length <= 3 ? true : false}
+            >
+              Entrar
             </button>
           </form>
         </div>
